2821af628ad2016c5ccc97aaf6e73a98586746ed
Alexander Shchukin <1340068@gmail.com>
2018-02-12 12:00:27 +0300

    search: - more cleared code

--- frontend/views/search/_result.php
+++ frontend/views/search/_result.php
@@ -5,10 +5,6 @@ use yii\helpers\Url;
 
 /* @var $this \yii\web\View */
 /* @var $model \common\models\Search */
-/* @var $items \common\models\Item[] */
-/* @var $error string */
-/* @var $q string */
-/* @var $dict array */
 
 ?>
 
@@ -19,14 +15,15 @@ use yii\helpers\Url;
 	<ul>
 		<? foreach ($model->models as $data) : ?>
 			<?
-				$img = $data->category->image->filename;
+				/** @var \common\models\Item $data */
+				$image = $data->category->image ? $data->category->image->getUrl() : '/img/category/empty.png';
 				$text_part = $model->text_part($data->body_purified, $model->dict);
 			?>
 
 			<li class="result">
 
 				<a href="<?= Url::to(['catalog/view', 'url' => $data->url]) ?>">
-					<img src="/img/category/<?= $img ? $img : 'empty.png' ?>">
+					<img src="<?= $image ?>">
 					<?= $data->header ?>
 					<?= $model->bold(strip_tags($data->header), $model->dict) ?>
 				</a>
