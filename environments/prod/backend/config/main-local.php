744dd62e034b83b9f58ae6da0d0b835cc95805b3
Alexander Shchukin <1340068@gmail.com>
2018-02-13 10:43:03 +0300

    - add yii2-debug to production when active debug mode - fix env: add yii2-debug config, prepare index.php

--- environments/prod/backend/config/main-local.php
+++ environments/prod/backend/config/main-local.php
@@ -1,5 +1,5 @@
 <?php
-return [
+$config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -7,3 +7,13 @@ return [
         ],
     ],
 ];
+
+if (YII_DEBUG && !YII_ENV_TEST) {
+	// configuration adjustments for 'debug' mode
+	$config['bootstrap'][] = 'debug';
+	$config['modules']['debug'] = [
+		'class' => 'yii\debug\Module',
+	];
+}
+
+return $config;
--- environments/prod/backend/web/index.php
+++ environments/prod/backend/web/index.php
@@ -1,6 +1,17 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', false);
-defined('YII_ENV') or define('YII_ENV', 'prod');
+
+$allow_ip = [
+	'127.0.0.1',
+	// add more ip4 and ip6 here to allow debug mode
+];
+if (in_array($_SERVER['REMOTE_ADDR'], $allow_ip)) {
+	defined('YII_DEBUG') or define('YII_DEBUG', true);
+	defined('YII_ENV') or define('YII_ENV', 'dev');
+} else {
+	// default:
+	defined('YII_DEBUG') or define('YII_DEBUG', false);
+	defined('YII_ENV') or define('YII_ENV', 'prod');
+}
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
