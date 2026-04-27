fc62a254e3911f2aeb785678c54c84a3b137b923
Alexander Shchukin <1340068@gmail.com>
2026-04-27 17:13:15 +0300

    update phones

--- common/config/params.php
+++ common/config/params.php
@@ -5,8 +5,9 @@ return [
     // 'user.passwordResetTokenExpire' => 3600,
 
     'phones' => [
-	    '+7&nbsp;(812)&nbsp;702-70-91',
-	    '+7&nbsp;(812)&nbsp;702-70-92',
+	    /*'+7&nbsp;(812)&nbsp;702-70-91',
+	    '+7&nbsp;(812)&nbsp;702-70-92',*/
+	    '+7&nbsp;(812)&nbsp;468-85-82',
     ],
     'feedback' => 'https://intermehanika-ltd.ru/feedback',
     'cacheTime' => 60*60 * 1, // 1 hour
